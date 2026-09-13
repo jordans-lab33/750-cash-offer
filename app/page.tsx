73b39891e830d0e5312a889e86f943f21527b84f
jordans-lab33 <aianalyzing@gmail.com>
2026-09-13 12:04:35 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 // ============================================
 // CUSTOMIZE YOUR REDIRECT LINK HERE:
-const REDIRECT_URL = "https://giftclick.org/aff_c?offer_id=1157&aff_id=147515";
+const REDIRECT_URL = "https://giftclick.org/aff_c?offer_id=3004&aff_id=147515";
 // ============================================
 
 const steps = [
